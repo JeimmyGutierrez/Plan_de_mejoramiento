e0d8a3a09194c7980765230ade4512dd10694e38
JeimmyGutierrez <116227825+JeimmyGutierrez@users.noreply.github.com>
2023-01-12 23:18:00 -0500

    Add files via upload
    Plan de mejoramiento completamente terminado.

---
+++ ejercicio1.py
@@ -0,0 +1,30 @@
+'''d={"gato":'chat','perro':'chien'.'caballo':'cheval'}
+funcion (d)'''
+
+
+
+'''def agregar (dictionary):
+    for f in dictionary.items():
+        print (f)
+d={'i':1,'o':2}
+o={'e':3,'p':4}
+o.update (d)
+print(d,o)'''
+
+
+
+'''def traduccion (dictionary):
+    for i in dictionary.values():
+        print(i)
+saludos={'hola':'ciao',
+'adios':'addio',
+'buenos días':'guete morge'}
+traduccion(saludos)'''
+
+'''def traduccion(dictionary):
+    for i in dictionary.keys():
+        print (i)
+ingles={'apple':'manzana',
+    'orange':'naranja',
+    'strawberry':'fresa'}
+traduccion(ingles)'''
